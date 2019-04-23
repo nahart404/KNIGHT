8d7f9f694a404531f229c7d63126089772055055
nahart <nahart404@gmail.com>
2019-04-23 13:10:54 -0700

    Fixed "Wonky" chain bug
    Added new script for the last chain link in the group of chains (the one connecting to the ball) to update it's location to the ball. Now reseting level doesn't cause strange chain positioning

--- Assets/Scripts/AnchorChain.cs
+++ Assets/Scripts/AnchorChain.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class AnchorChain : MonoBehaviour
 {
+    /* script used to anchor first chain in the line to the moving paddle
+     */
     //config
-    //need both paddle and ball class
     [SerializeField] Paddle paddle1;
     [SerializeField] Ball ball1;
     [SerializeField] Sprite chainSprite; //the sprite to use
 
     //variables
-    Vector2 paddlePosition;
-    Vector2 ballPosition;
-    Vector2 dist; //distance between ball and paddle
+    //Vector2 paddlePosition;
+    //Vector2 ballPosition;
+    //Vector2 dist; //distance between ball and paddle
     Vector2 currentPosition;
 
 
@@ -27,14 +28,13 @@ public class AnchorChain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y); //pos of paddle
-        ballPosition = new Vector2(ball1.transform.position.x, ball1.transform.position.y); //pos of ball
-        dist = ballPosition - paddlePosition;
+        //paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y); //pos of paddle
+        //ballPosition = new Vector2(ball1.transform.position.x, ball1.transform.position.y); //pos of ball
+        //dist = ballPosition - paddlePosition;
 
         currentPosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y); //new chain position
         transform.position = currentPosition;
 
-        //drawSprite(dist);
 
     }
 }
---
+++ Assets/Scripts/BallLink.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLink : MonoBehaviour
+{
+    /*Script used to link the last chain link of the group to the ball to avoid the strange physics bug after 
+     * reseting a level
+     */ 
+    //config
+    [SerializeField] Ball ball1;
+
+    //variables
+    Vector2 currentPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPosition = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPosition = new Vector2(ball1.transform.position.x, ball1.transform.position.y); //new chain position
+        transform.position = currentPosition;
+    }
+}
